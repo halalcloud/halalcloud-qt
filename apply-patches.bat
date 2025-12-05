8b79407a43ab6b72ebbbeb0d5c66cd9b8aaa5c17
MouriNaruto <Kenji.Mouri@outlook.com>
2025-12-05 14:12:46 +0800

    Add apply-patches.bat.

---
+++ apply-patches.bat
@@ -0,0 +1,15 @@
+@setlocal
+
+@echo off
+
+set PatchesFolder="%~dp0Patches"
+set QtBaseFolder="%~dp0qtbase"
+
+pushd %QtBaseFolder%
+git am %PatchesFolder%\MileWindowsUniCrtIntegration.patch
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+git am %PatchesFolder%\QWindowsFontDatabaseWithoutDirectWrite.patch
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+popd
+
+@endlocal
