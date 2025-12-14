8029c010a152518ed9b33f335ed90b8b73490644
MouriNaruto <Kenji.Mouri@outlook.com>
2025-12-14 15:37:37 +0800

    Add Patches\QMakeVisualStudio2026Support.patch.

--- apply-patches.bat
+++ apply-patches.bat
@@ -10,6 +10,8 @@ git am %PatchesFolder%\MileWindowsUniCrtIntegration.patch
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 git am %PatchesFolder%\QWindowsFontDatabaseWithoutDirectWrite.patch
 if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+git am %PatchesFolder%\QMakeVisualStudio2026Support.patch
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 popd
 
 @endlocal
